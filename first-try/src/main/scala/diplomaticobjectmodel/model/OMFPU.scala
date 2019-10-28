c65f6b6ea7c53a680f9fd6a05d3fcba740203932
CS Unix Lab <csunixlab@gee.cs.williams.edu>
2019-10-27 21:22:27 -0400

    removing first try

--- first-try/src/main/scala/diplomaticobjectmodel/model/OMFPU.scala
+++
@@ -1,8 +0,0 @@
-// See LICENSE.SiFive for license details.
-
-package freechips.rocketchip.diplomaticobjectmodel.model
-
-case class OMFPU(
-  fLen: Int,
-  _types: Seq[String] = Seq("OMFPU", "OMComponent", "OMCompoundType")
-) extends OMComponent
