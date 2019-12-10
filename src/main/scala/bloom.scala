1bbdf8de1836fb47b34c70d5e2440aa11472d762
joshuaminwookang <jmk@jmks-MacBook-Air.local>
2019-12-09 22:27:19 -0500

    testing

--- src/main/scala/bloom.scala
+++ src/main/scala/bloom.scala
@@ -48,10 +48,11 @@ class BloomAccelImp(outer: BloomAccel)(implicit p: Parameters) extends LazyRoCCM
       bloom_bit_array := mapModule.io.output_hashBits 
       printf("I'm mapping !!!! %d\n", hashed_string)
     }
-    when (doTest) {
+    when (doTest) 
       testModule.io.input_value := hashed_string
       testModule.io.input_bit_array := bloom_bit_array
-      miss_counter := Mux(testModule.io.output_boolean, miss_counter, miss_counter+1.U(64.W))
+      //miss_counter := Mux(testModule.io.output_boolean, miss_counter, miss_counter+1.U(64.W))
+      miss_counter := miss_counter+1.U(64.W))
     }
   }
 
