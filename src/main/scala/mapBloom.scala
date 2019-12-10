c1273b2b57a75123742dfe4bf6deba84d2b28766
joshuaminwookang <jmk@jmks-MacBook-Air.local>
2019-12-10 11:05:44 -0500

    testing

--- src/main/scala/mapBloom.scala
+++ src/main/scala/mapBloom.scala
@@ -18,6 +18,10 @@ class MapBloomModule(val M: Int, val K: Int) extends Module {
     val output_hashIndex = Output(UInt(64.W))
     val output_busy = Output(Bool())
   }
+    // states of this module 
+    val s_idle :: s_hash :: s_resp :: Nil = Enum(Bits(), 3)
+    val state_reg = RegInit(s_idle)
+
      // Local variables
     val x  = RegInit(0.U(64.W))
     val y  = RegInit(0.U(64.W))
@@ -25,17 +29,51 @@ class MapBloomModule(val M: Int, val K: Int) extends Module {
     val done = (i === K.asUInt(64.W))
   
     // Hash computation
-    when(io.input_reset){
-      i := RegInit(0.U(64.W))
-      x := io.input_value
-      y := io.input_value >> 4
-    } otherwise {
-      i := i + 1.U(64.W)
-      x := Mux(~done, (x + y) % K.asUInt(64.W), x)  
-      y := Mux(~done, (y + i) % K.asUInt(64.W), y)
-      io.output_hashBits(x) := 1.U(1.W)
-      io.output_hashIndex := x
-    }
+    // when(io.input_reset){
+    //   i := RegInit(0.U(64.W))
+    //   x := io.input_value
+    //   y := io.input_value >> 4
+    // } otherwise {
+    //   i := i + 1.U(64.W)
+    //   x := Mux(~done, (x + y) % K.asUInt(64.W), x)  
+    //   y := Mux(~done, (y + i) % K.asUInt(64.W), y)
+    //   io.output_hashBits(x) := 1.U(1.W)
+    //   io.output_hashIndex := x
+    // }
 
     io.output_busy := !done
+
+    switch(state_reg) {
+      is (s_idle) {
+        when(io.input_reset){
+          i := RegInit(0.U(64.W))
+          x := io.input_value
+          y := io.input_value >> 4
+          io.output_hashBits(x) := 1.U(1.W)
+          io.output_hashIndex := x
+          done := (i === K.asUInt(64.W)) || (bit === 0.U(1.W))
+          // get into hashing state
+          state_reg := s_hash
+        }
+      }
+      is (s_hash) {
+        i := i + 1.U(64.W)
+        x := (x + y) % K.asUInt(64.W)  
+        y := (y + i) % K.asUInt(64.W
+        io.output_hashBits(x) := 1.U(1.W)
+        io.output_hashIndex := x
+        done := (i === K.asUInt(64.W)) || (bit === 0.U(1.W))
+
+        // are we done hashing?
+        when (done) {
+          state_reg := s_resp
+        }
+      }
+      // respond to main module 
+      is (s_resp) {
+        io.output_hashBits(x) := 1.U(1.W)
+        io.output_hashIndex := x
+      }
+    }
+}
 }
--- src/main/scala/testBloom.scala
+++ src/main/scala/testBloom.scala
@@ -18,27 +18,46 @@ class TestBloomModule(val M: Int, val K: Int) extends Module {
     val output_bit = Output(UInt(1.W))
     val output_busy = Output(Bool())
   }
+    // states of this module 
+    val s_idle :: s_hash :: s_resp :: Nil = Enum(Bits(), 3)
+    val state_reg = RegInit(s_idle)
+
      // Local variables
     val x  = RegInit(0.U(64.W))
     val y  = RegInit(0.U(64.W))
     val i  = RegInit(0.U(64.W))
     val bit = RegInit(1.U(1.W))
     val done = (i === K.asUInt(64.W)) || (bit === 0.U(1.W))
-  
-    // Hash computation
-    when(io.input_reset){
-      i := RegInit(0.U(64.W))
-      x := io.input_value
-      y := io.input_value >> 4
-      bit := RegInit(1.U(1.W))
-      io.output_bit := 0.U(1.W)
-    } otherwise {
-      i := i + 1.U(64.W)
-      x := Mux(~done, (x + y) % K.asUInt(64.W), x)  
-      y := Mux(~done, (y + i) % K.asUInt(64.W), y)
-      bit := io.input_bit_array(x)
-      io.output_bit := ~bit
-    }
 
     io.output_busy := !done
+
+    switch(state_reg) {
+      is (s_idle) {
+        when(io.input_reset){
+          i := RegInit(0.U(64.W))
+          x := io.input_value
+          y := io.input_value >> 4
+          bit := RegInit(1.U(1.W))
+          io.output_bit := 0.U(1.W)
+          done := (i === K.asUInt(64.W)) || (bit === 0.U(1.W))
+          // get into hashing state
+          state_reg := s_hash
+        }
+      }
+      is (s_hash) {
+        i := i + 1.U(64.W)
+        x := (x + y) % K.asUInt(64.W)  
+        y := (y + i) % K.asUInt(64.W
+        bit := io.input_bit_array(x)
+        io.output_bit := ~bit
+        done := (i === K.asUInt(64.W)) || (bit === 0.U(1.W))
+        when (done) {
+          state_reg := s_resp
+        }
+      }
+      is (s_resp) {
+        bit := io.input_bit_array(x)
+        io.output_bit := ~bit
+      }
+    }
 }
