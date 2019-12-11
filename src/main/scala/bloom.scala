81435eae92cf571c59899ad067da0d821c9fc399
joshuaminwookang <jmk@jmks-MacBook-Air.local>
2019-12-11 17:21:55 -0500

    hardcoded map bloom

--- src/main/scala/bloom.scala
+++ src/main/scala/bloom.scala
@@ -38,27 +38,27 @@ class BloomAccelImp(outer: BloomAccel)(implicit p: Parameters) extends LazyRoCCM
 
   // val mapModule = Module(new MapBloomModule(outer.m,outer.k))
   // val testModule = Module(new TestBloomModule(outer.m,outer.k)) 
-  // val mapModule = Module(new MapBloomModule)
-  // val testModule = Module(new TestBloomModule)
+  val mapModule = Module(new MapBloomModule)
+  val testModule = Module(new TestBloomModule)
 
   // Hash computation
-  val x0  = RegInit(0.U(64.W))
-  val y0  = RegInit(0.U(64.W))
+  // val x0  = RegInit(0.U(64.W))
+  // val y0  = RegInit(0.U(64.W))
 
-  val x1  = RegInit(0.U(64.W))
-  val y1  = RegInit(0.U(64.W))
+  // val x1  = RegInit(0.U(64.W))
+  // val y1  = RegInit(0.U(64.W))
 
-  val x2  = RegInit(0.U(64.W))
-  val y2  = RegInit(0.U(64.W))
+  // val x2  = RegInit(0.U(64.W))
+  // val y2  = RegInit(0.U(64.W))
 
-  val x3  = RegInit(0.U(64.W))
-  val y3  = RegInit(0.U(64.W))
+  // val x3  = RegInit(0.U(64.W))
+  // val y3  = RegInit(0.U(64.W))
 
-  val x4  = RegInit(0.U(64.W))
-  val y4  = RegInit(0.U(64.W))
+  // val x4  = RegInit(0.U(64.W))
+  // val y4  = RegInit(0.U(64.W))
 
-  val x5  = RegInit(0.U(64.W))
-  val y5  = RegInit(0.U(64.W))
+  // val x5  = RegInit(0.U(64.W))
+  // val y5  = RegInit(0.U(64.W))
 
   // val x0  = hashed_string
   // val y0  = hashed_string >> 4.U(64.W)
@@ -78,74 +78,72 @@ class BloomAccelImp(outer: BloomAccel)(implicit p: Parameters) extends LazyRoCCM
   // val x5  = (x4 + y4) % bloom_param_m 
   // val y5  = (y4 + 4.U(64.W)) % bloom_param_m 
 
-  x0 := hashed_string
-  y0 := hashed_string >> 4
+  // x0 := hashed_string
+  // y0 := hashed_string >> 4
 
-  x1 := (x0 + y0) % 20000.U(64.W)
-  y1 := (y0 + 0.U(64.W)) % 20000.U(64.W)
+  // x1 := (x0 + y0) % 20000.U(64.W)
+  // y1 := (y0 + 0.U(64.W)) % 20000.U(64.W)
 
-  x2 := (x1 + y1) % 20000.U(64.W)
-  y2 := (y1 + 1.U(64.W)) % 20000.U(64.W)
+  // x2 := (x1 + y1) % 20000.U(64.W)
+  // y2 := (y1 + 1.U(64.W)) % 20000.U(64.W)
 
-  x3 := (x2 + y2) % 20000.U(64.W)
-  y3 := (y2 + 2.U(64.W)) % 20000.U(64.W)
+  // x3 := (x2 + y2) % 20000.U(64.W)
+  // y3 := (y2 + 2.U(64.W)) % 20000.U(64.W)
 
-  x4 := (x3 + y3) % 20000.U(64.W)
-  y4 := (y3 + 3.U(64.W)) % 20000.U(64.W)
+  // x4 := (x3 + y3) % 20000.U(64.W)
+  // y4 := (y3 + 3.U(64.W)) % 20000.U(64.W)
 
-  x5 := (x4 + y4) % 20000.U(64.W)
-  y5 := (y4 + 4.U(64.W)) % 20000.U(64.W)
+  // x5 := (x4 + y4) % 20000.U(64.W)
+  // y5 := (y4 + 4.U(64.W)) % 20000.U(64.W)
 
-  val found1 = RegInit(1.U(1.W))
-  val found2 = RegInit(1.U(1.W))
-  val found3 = RegInit(1.U(1.W))
-  val found4 = RegInit(1.U(1.W))
-  val found5 = RegInit(1.U(1.W))
+  // val found1 = RegInit(1.U(1.W))
+  // val found2 = RegInit(1.U(1.W))
+  // val found3 = RegInit(1.U(1.W))
+  // val found4 = RegInit(1.U(1.W))
+  // val found5 = RegInit(1.U(1.W))
 
-  found1 := bloom_bit_array(x1)
-  found2 := bloom_bit_array(x2)
-  found3 := bloom_bit_array(x3)
-  found4 := bloom_bit_array(x4)
-  found5 := bloom_bit_array(x5)
+  // found1 := bloom_bit_array(x1)
+  // found2 := bloom_bit_array(x2)
+  // found3 := bloom_bit_array(x3)
+  // found4 := bloom_bit_array(x4)
+  // found5 := bloom_bit_array(x5)
 
   // Custom function behaviors
-  when (cmd.fire()) {
-    when (doInit) {
-      bloom_bit_array := Reg(init = Vec.fill(20000)(0.U(1.W)))
-      miss_counter := RegInit(0.U(64.W))
-    }
-    when (doMap) {
-      bloom_bit_array(x1) := 1.U(1.W)
-      bloom_bit_array(x2) := 1.U(1.W)
-      bloom_bit_array(x3) := 1.U(1.W)
-      bloom_bit_array(x4) := 1.U(1.W)
-      bloom_bit_array(x5) := 1.U(1.W)
-    } 
-    when (doTest) {
-      miss_counter := miss_counter + (found1 & found2 & found3 & found4 & found5)
-      // miss_counter := miss_counter+1.U(64.W))
-    } 
-  } 
-
   // when (cmd.fire()) {
   //   when (doInit) {
   //     bloom_bit_array := Reg(init = Vec.fill(20000)(0.U(1.W)))
   //     miss_counter := RegInit(0.U(64.W))
   //   }
   //   when (doMap) {
-  //     mapModule.io.input_value := hashed_string
-  //     // mapModule.io.input_reset := true.B
-  //     bloom_bit_array <> mapModule.io.output_hashBits 
-  //     map_counter := mapModule.io.output_hashIndex
+  //     bloom_bit_array(x1) := 1.U(1.W)
+  //     bloom_bit_array(x2) := 1.U(1.W)
+  //     bloom_bit_array(x3) := 1.U(1.W)
+  //     bloom_bit_array(x4) := 1.U(1.W)
+  //     bloom_bit_array(x5) := 1.U(1.W)
   //   } 
   //   when (doTest) {
-  //     testModule.io.input_value := hashed_string
-  //     testModule.io.input_bit_array <> bloom_bit_array
-  //     // testModule.io.input_reset := true.B
-  //     miss_counter := miss_counter + testModule.io.output_found
-  //     // miss_counter := miss_counter+1.U(64.W))
+  //     miss_counter := miss_counter + (found1 & found2 & found3 & found4 & found5)
   //   } 
   // } 
+
+  when (cmd.fire()) {
+    when (doInit) {
+      bloom_bit_array := Reg(init = Vec.fill(20000)(0.U(1.W)))
+      miss_counter := RegInit(0.U(64.W))
+    }
+    when (doMap) {
+      mapModule.io.input_value := hashed_string
+      // mapModule.io.input_reset := true.B
+      bloom_bit_array <> mapModule.io.output_hashBits 
+      map_counter := mapModule.io.output_hashIndex
+    } 
+    when (doTest) {
+      testModule.io.input_value := hashed_string
+      testModule.io.input_bit_array <> bloom_bit_array
+      // testModule.io.input_reset := true.B
+      miss_counter := miss_counter + testModule.io.output_found
+    } 
+  } 
 
   // busy := mapModule.io.output_busy || testModule.io.output_busy
 
