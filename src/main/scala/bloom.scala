f2f311f7ee33b622354edb091f84f2b7c5658c14
joshuaminwookang <jmk@jmks-MacBook-Air.local>
2019-12-11 23:17:14 -0500

    hardcoded map bloom

--- src/main/scala/bloom.scala
+++ src/main/scala/bloom.scala
@@ -21,7 +21,7 @@ class BloomAccel(opcodes: OpcodeSet)
 
 class BloomAccelImp(outer: BloomAccel)(implicit p: Parameters) extends LazyRoCCModuleImp(outer) {
   // accelerator memory 
-  val bloom_bit_array = Reg(VecInit(Seq.fill(20000)(0.U(1.W))))
+  val bloom_bit_array = RegInit(VecInit(Seq.fill(20000)(0.U(1.W))))
   val miss_counter = RegInit(0.U(64.W))
   val busy = RegInit(Bool(false))
 
@@ -130,7 +130,7 @@ class BloomAccelImp(outer: BloomAccel)(implicit p: Parameters) extends LazyRoCCM
 
   when (cmd.fire()) {
     when (doInit) {
-      bloom_bit_array := Reg(VecInit(Seq.fill(20000)(0.U(1.W))))
+      bloom_bit_array := RegInit(VecInit(Seq.fill(20000)(0.U(1.W))))
       miss_counter := RegInit(0.U(64.W))
     }
     when (doMap) {
